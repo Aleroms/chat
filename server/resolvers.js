e210a2e74d5936915f79b91985acde53a9d223bb
Santiago <santiagomorales333@yahoo.com>
2024-02-16 14:04:26 -0800

    created web sockets using graphql

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,8 @@
-import { GraphQLError } from 'graphql';
-import { createMessage, getMessages } from './db/messages.js';
+import { GraphQLError } from "graphql";
+import { createMessage, getMessages } from "./db/messages.js";
+import { PubSub } from "graphql-subscriptions";
+
+const pubSub = new PubSub();
 
 export const resolvers = {
   Query: {
@@ -10,15 +13,22 @@ export const resolvers = {
   },
 
   Mutation: {
-    addMessage: (_root, { text }, { user }) => {
+    addMessage: async (_root, { text }, { user }) => {
       if (!user) throw unauthorizedError();
-      return createMessage(user, text);
+      const message = await createMessage(user, text);
+      pubSub.publish("message_added", { messageAdded: message });
+      return message;
+    },
+  },
+  Subscription: {
+    messageAdded: {
+      subscribe: () => pubSub.asyncIterator("message_added"),
     },
   },
 };
 
 function unauthorizedError() {
-  return new GraphQLError('Not authenticated', {
-    extensions: { code: 'UNAUTHORIZED' },
+  return new GraphQLError("Not authenticated", {
+    extensions: { code: "UNAUTHORIZED" },
   });
 }
